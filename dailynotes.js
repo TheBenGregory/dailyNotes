f83a6ac9fa390e52e84f70276f65b0334a251cc7
Ben Gregory <JBGregoryPro@protonmail.com>
2021-07-16 10:52:20 -0500

    a neat nest of notes for node

---
+++ dailynotes.js
@@ -0,0 +1,28 @@
+const notes = [
+{
+    id: 1,
+    subject: "gitHub",
+    date: "12 JUL 2021",
+    feeling: "capable",
+    timeSpent: '8 hours'
+},
+{
+    id: 2,
+    subject: "JavaScript",
+    date: "14 JUL 2021",
+    feeling: "like a novice",
+    timeSpent: "16 hours"
+},
+{
+    id: 3,
+    subject: "CSS",
+    date: "11 JUL 2021",
+    feeling: "like I'm getting there",
+    timeSpent: "20 hours"
+}
+]
+
+for (const item of notes) {
+        console.log(`Today i worked on ${item.subject} and I feel ${item.feeling}`);
+    
+}
